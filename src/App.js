5e481541c9b256c6df9fbd27b3ead97042c649f3
MarcuAdrian18 <80761236+MarcuAdrian18@users.noreply.github.com>
2022-08-29 14:39:30 +0300

    I just added the color boxes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import seedColors from "./seedColors";
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[4]} />
+      <Palette {...seedColors[2]} />
     </div>
   );
 }
