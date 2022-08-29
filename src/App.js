657af4ca200cb69491c6a24413c7d2cdc1800b06
MarcuAdrian18 <80761236+MarcuAdrian18@users.noreply.github.com>
2022-08-29 14:13:13 +0300

    First commit - I created the Palette Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[4]} />
+    </div>
+  );
+}
+
+export default App;
