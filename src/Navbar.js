3a2dc753c7f53b230920af798f9aa232a472cb92
MarcuAdrian18 <80761236+MarcuAdrian18@users.noreply.github.com>
2022-08-30 16:13:48 +0300

    Material UI dropdown menu was added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import Select from "@mui/material/Select/Select";
+import MenuItem from "@mui/material/MenuItem";
 import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex", open: false };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+  }
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true });
+    this.props.handleChange(e.target.value);
+  }
   render() {
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -21,6 +33,13 @@ class Navbar extends Component {
               onAfterChange={this.props.changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
--- src/Palette.js
+++ src/Palette.js
@@ -6,19 +6,29 @@ import "./Palette.css";
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
   changeLevel(level) {
     this.setState({ level });
   }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
   render() {
+    const { level, format } = this.state;
     const colorBoxes = this.props.palette.colors[this.state.level].map(
-      (color) => <ColorBox background={color.hex} name={color.name} />
+      (color) => <ColorBox background={color[format]} name={color.name} />
     );
     return (
       <div className="Palette">
-        <Navbar level={this.state.level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+          showingAllColors
+        />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer goes here */}
       </div>
