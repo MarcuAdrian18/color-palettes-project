9c1e3ee2986976d4ace878fba5d5d5e42aeb9f24
MarcuAdrian18 <80761236+MarcuAdrian18@users.noreply.github.com>
2022-08-30 17:34:19 +0300

    Added React Router but the 5.2.0 version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Palette from "./Palette";
+import { Route, Switch } from "react-router-dom";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
   // colorHelpers.js in action => console.log(generatePalette(seedColors[4]));
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[2])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>INDIVIDUAL PALETTE</h1>}
+      />
+    </Switch>
+    // <div className="App">
+
+    //   <Palette palette={generatePalette(seedColors[2])} />
+    // </div>
   );
 }
 
