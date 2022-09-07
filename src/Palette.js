a3808de6aa0c42c92d5442b38c6ebf8b79e9470a
MarcuAdrian18 <80761236+MarcuAdrian18@users.noreply.github.com>
2022-09-07 10:09:22 +0300

    Refactoring some styles with JSS conditionals

--- src/Palette.js
+++ src/Palette.js
@@ -29,7 +29,7 @@ class Palette extends Component {
           id={color.id}
           paletteId={id}
           moreUrl={`palette/${id}/${color.id}`}
-          showLink={true}
+          showingFullPalette={true}
         />
       )
     );
